6de3c3e43a505c1a8b56676ede7e4d6d3de5fb86
狐白吟 <114640011+hubaiyin@users.noreply.github.com>
2023-08-19 14:37:54 +0800

    手动更改

--- src/views/IndexVc.vue
+++ src/views/IndexVc.vue
@@ -1,8 +1,33 @@
 <template >
   <dv-full-screen-container>
     <div class="screen">
-      <div class="header">this is header</div>
-      <div class="middle">this is middle</div>
+      <div class="header">
+        <div class="after">
+          <router-link to="/before/monitor">监控画面</router-link>
+        </div>
+        <div class="after">
+          <router-link to="/before/device">设备管理</router-link>
+        </div>
+        <div class="after">
+          <router-link to="/before/data">数据监测</router-link>
+        </div>
+        <div class="main">
+          <router-link to="/before/home">海上光伏数据大屏</router-link>
+        </div>
+        <div class="after">
+          <router-link to="/before/warn">预警管理</router-link>
+        </div>
+        <div class="after">
+          <router-link to="/before/report">报表管理</router-link>
+        </div>
+        <div class="after">
+          <router-link to="/before/log">系统日志</router-link>
+        </div>
+      </div>
+      <div class="middle">
+        <div class="time">2023年11月4日 14:51</div>
+        <div class="login">登录｜注册</div>
+      </div>
       <div class="container">
         <router-view></router-view>
       </div>
@@ -26,16 +51,59 @@ export default {
     box-sizing: border-box;
   }
   .header {
-    height: 7%;
+    height: 9%;
     background: pink;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    .main,
+    .after {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 94%;
+      background: rosybrown;
+    }
+
+    .main {
+      font-size: 22px;
+      width: 30%;
+    }
+    .after {
+      height: 60%;
+      width: 10%;
+      background: skyblue;
+    }
   }
   .middle {
-    height: 4%;
+    height: 5%;
     background: skyblue;
+    display: flex;
+    justify-content: space-between;
+    box-sizing: border-box;
+    padding: 0 2%;
+    align-items: center;
+    .time {
+      height: 100%;
+      width: 15%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
   }
   .container {
-    flex: 1;
+    height: 86%;
     background: orange;
+  }
+
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+
+  .router-link-active {
+    text-decoration: none;
   }
 }
 </style>
