34a2d092849f832f9a29a94733f9f361867a15a6
狐白吟 <114640011+hubaiyin@users.noreply.github.com>
2023-10-20 13:36:47 +0800

    Hu bai yin (#13)
    * 手动更改
    
    * 手动更改
    
    * 测试
    
    * 实时监测架子搭建一大半
    
    * 面包屑的制作
    
    * 合并 (#1)
    
    test
    
    * 颜色搭配
    
    * 样式bug修复
    
    * 实时监测架子加基础样式
    
    * 实时监测架子加基础样式
    
    * Hu bai yin (#2)
    
    * 颜色搭配
    
    * 样式bug修复
    
    * 实时监测架子加基础样式
    
    * 奇怪的样式
    
    * 奇怪的样式
    
    * Hu bai yin (#3)
    
    * 实时监测架子加基础样式
    
    * 奇怪的样式
    
    * 实时监测基本搭建完成
    
    * 实时监测基本搭建完成
    
    * Hu bai yin (#4)
    
    * 实时监测架子加基础样式
    
    * 奇怪的样式
    
    * 实时监测基本搭建完成
    
    * 图表边框
    
    * 图表边框
    
    * Hu bai yin (#5)
    
    * 实时监测架子加基础样式
    
    * 奇怪的样式
    
    * 实时监测基本搭建完成
    
    * 图表边框
    
    * header更改
    
    * 我也不知道是啥
    
    * 我也不知道是啥
    
    * Hu bai yin (#6)
    
    * 实时监测架子加基础样式
    
    * 奇怪的样式
    
    * 实时监测基本搭建完成
    
    * 图表边框
    
    * 我也不知道是啥
    
    * 错的字体大小
    
    * nothing
    
    * nothing
    
    * 下拉框样式
    
    * 下拉框样式
    
    * Hu bai yin (#8)
    
    * 实时监测架子加基础样式
    
    * 奇怪的样式
    
    * 实时监测基本搭建完成
    
    * 图表边框
    
    * 我也不知道是啥
    
    * nothing
    
    * 下拉框样式
    
    * 特殊颜色的折线图封装
    
    * 特殊颜色的折线图封装
    
    * Hu bai yin (#9)
    
    * 实时监测架子加基础样式
    
    * 奇怪的样式
    
    * 实时监测基本搭建完成
    
    * 图表边框
    
    * 我也不知道是啥
    
    * nothing
    
    * 下拉框样式
    
    * 特殊颜色的折线图封装
    
    * 历史数据静态 (#10)
    
    * 历史数据对图表样式略微修改
    
    * 历史数据对图表样式略微修改
    
    * 没什么
    
    * 没什么
    
    * 提交
    
    * 缩放
    
    * 缩放
    
    * 一些样式
    
    * 一些样式
    
    * Hu bai yin (#11)
    
    * 实时监测架子加基础样式
    
    * 奇怪的样式
    
    * 实时监测基本搭建完成
    
    * 图表边框
    
    * 我也不知道是啥
    
    * nothing
    
    * 下拉框样式
    
    * 特殊颜色的折线图封装
    
    * 历史数据对图表样式略微修改
    
    * 没什么
    
    * 缩放
    
    * 一些样式
    
    * 背景
    
    * 防止诡异的报错
    
    * 为了切换分支
    
    * 画图
    
    * 首页搭建完毕
    
    * gitignore
    
    * 解决合并bug
    
    * 没啥，地图颜色
    
    ---------
    
    Co-authored-by: Yowo-moran <123151768+Yowo-moran@users.noreply.github.com>
    Co-authored-by: AwayHoper <1508262532@qq.com>
    Co-authored-by: Awayer <119123081+AwayHoper@users.noreply.github.com>
    Co-authored-by: 狐白吟 <hubaiyin@hubaiyindeMacBook-Air.local>

--- src/components/MapVc.vue
+++ src/components/MapVc.vue
@@ -29,6 +29,9 @@ export default {
   methods: {
     initMap() {
       console.log("hi");
+      window._AMapSecurityConfig = {
+        securityJsCode: "b387338487cee9e1a35360c3210cca25",
+      };
       AMapLoader.load({
         key: "593c04957cb7cfa503dd408e55afdbd4",
         version: "1.4.0",
@@ -37,6 +40,7 @@ export default {
         this.map = new AMap.Map("container", {
           zoom: 3.4,
           center: [106.397428, 31.90923],
+          mapStyle: "amap://styles/darkblue",
         });
         for (let i = 0; i < this.positions.length; i++) {
           // 创建一个 Icon
